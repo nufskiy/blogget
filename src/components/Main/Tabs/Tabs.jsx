176307392312ec8133c035b5b5206c63880fc432
nufskiy <nufskiy@rambler.ru>
2023-11-07 01:30:09 +0300

    debounce + adaptive + IconComponents

--- src/components/Main/Tabs/Tabs.jsx
+++ src/components/Main/Tabs/Tabs.jsx
@@ -1,29 +1,71 @@
 import style from './Tabs.module.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {assignId} from '../../../utils/generateRandomId';
+import {debounceRaf} from '../../../utils/debounce';
+
+import {ReactComponent as ArrowIcon} from './img/arrow.svg';
+import {ReactComponent as HomeIcon} from './img/home.svg';
+import {ReactComponent as TopIcon} from './img/top.svg';
+import {ReactComponent as BestIcon} from './img/best.svg';
+import {ReactComponent as HotIcon} from './img/hot.svg';
 
 const LIST = [
-  {value: 'Главная'},
-  {value: 'Просмотренные'},
-  {value: 'Сохраненные'},
-  {value: 'Мои посты'},
+  {value: 'Главная', Icon: HomeIcon},
+  {value: 'Топ', Icon: TopIcon},
+  {value: 'Лучшие', Icon: BestIcon},
+  {value: 'Горячие', Icon: HotIcon},
 ].map(assignId);
 
 export const Tabs = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isDropdown, setIsDropdown] = useState(true);
+  const [currentValue, setCurrentValue] = useState(LIST[0].value);
+
+  const handleResize = () => {
+    if (document.documentElement.clientWidth < 768) {
+      setIsDropdown(true);
+    } else {
+      setIsDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    const debounceResize = debounceRaf(handleResize);
+
+    handleResize();
+    window.addEventListener('resize', debounceResize);
+
+    return () => {
+      window.removeEventListener('resize', debounceResize);
+    };
+  }, []);
 
   return (
-    <>
-      <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
-        add item
-      </button>
+    <div className={style.container}>
+      {isDropdown && (
+        <div className={style.wrapperBtn}>
+          <button className={style.btn}
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+            {currentValue}
+            <ArrowIcon width={15} height={15} />
+          </button>
+        </div>
+      )}
 
-      {isDropdownOpen && <ul className={style.list}>
-        {LIST.map(({value, id}) => (
-          <li key={id}>
-            <button onClick={() => {}}>{value}</button>
-          </li>))}
-      </ul>}
-    </>
+      {(!isDropdown || isDropdownOpen) && (
+        <ul className={style.list}
+          onClick={() => setIsDropdownOpen(false)}>
+          {LIST.map(({value, id, Icon}) => (
+            <li className={style.item} key={id}>
+              <button className={style.btn}
+                onClick={() => setCurrentValue(value)}>
+                {value}
+                {Icon && <Icon width={30} height={30} />}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
