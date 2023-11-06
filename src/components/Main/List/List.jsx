a7aaa21b0b7e8cbd6f32ab6f78d774069a9cba5a
nufskiy <nufskiy@rambler.ru>
2023-11-06 22:41:21 +0300

    Todolist-like tabs

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,18 +2,48 @@ import style from './List.module.css';
 import Post from './Post';
 
 export const List = () => {
-  const postData = {
-    thumbnail: '',
-    // eslint-disable-next-line max-len
-    title: 'Title',
-    author: 'Nickname',
-    ups: 24,
-    date: '2022-02-24T09:45:00.000Z'
-  };
+  const postsData = [
+    {
+      thumbnail: '',
+      title: 'Title1',
+      author: 'Nickname1',
+      ups: 77,
+      date: '2022-01-22T00:45:00.000Z',
+      id: 123
+    },
+    {
+      thumbnail: '',
+      title: 'Title2',
+      author: 'Nickname2',
+      ups: 24,
+      date: '2022-01-31T03:33:00.000Z',
+      id: 456
+    },
+    {
+      thumbnail: '',
+      title: 'Title3',
+      author: 'Nickname3',
+      ups: 58,
+      date: '2022-02-24T09:45:00.000Z',
+      id: 789
+    },
+    {
+      thumbnail: '',
+      title: 'Title4',
+      author: 'Nickname4',
+      ups: 11,
+      date: '2022-03-11T14:15:00.000Z',
+      id: 768
+    },
+  ];
 
   return (
     <ul className={style.list}>
-      <Post postData={postData} />
+      {
+        postsData.map((postData) => (
+          <Post key={postData.id} postData={postData} />
+        ))
+      }
     </ul>
   );
 };
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,29 @@ import Layout from '../Layout';
 import style from './Main.module.css';
 import Tabs from './Tabs';
 import List from './List';
+import {useState} from 'react';
+import {assignId} from '../../utils/generateRandomId';
 
-export const Main = () => (
-  <main className={style.main}>
-    <Layout>
-      <Tabs />
-      <List />
-    </Layout>
-  </main>
-);
+const LIST = [
+  {value: 'Главная'},
+  {value: 'Просмотренные'},
+  {value: 'Сохраненные'},
+  {value: 'Мои посты'},
+].map(assignId);
+
+export const Main = () => {
+  const [list, setList] = useState(LIST);
+
+  const addItem = () => {
+    setList([...list, assignId({value: 'Новый пост'})]);
+  };
+
+  return (
+    <main className={style.main}>
+      <Layout>
+        <Tabs list={list} setList={setList} addItem={addItem} />
+        <List />
+      </Layout>
+    </main>
+  );
+};
